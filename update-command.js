24a4019faaaa0ca06df3384b633873d4b31c0792
Pier-Luc Brault <pier-luc@brault.me>
2022-03-13 13:57:27 -0400

    Use top-level await

--- update-command.js
+++ update-command.js
@@ -8,22 +8,20 @@ function extractList(googleAddrData) {
     .filter((addr) => !addr.startsWith('8.8'))
 }
 
-(async () => {
-  const googleAddrData = await (await fetch('https://www.gstatic.com/ipranges/goog.json')).json();
+const googleAddrData = await (await fetch('https://www.gstatic.com/ipranges/goog.json')).json();
 
-  const googleAddrList = extractList(googleAddrData);
+const googleAddrList = extractList(googleAddrData);
 
-  let command = '/ip firewall\n';
+let command = '/ip firewall\n';
 
-  googleAddrList.forEach((addr) => {
-    command += `address-list add list=google address=${addr}\n`;
-  });
+googleAddrList.forEach((addr) => {
+  command += `address-list add list=google address=${addr}\n`;
+});
 
-  fs.writeFile('command.txt', command, (err) => {
-    if (err) {
-      console.error(err)
-    } else {
-      console.log('File "command.txt" updated successfully.');
-    }
-  });
-})();
+fs.writeFile('command.txt', command, (err) => {
+  if (err) {
+    console.error(err)
+  } else {
+    console.log('File "command.txt" updated successfully.');
+  }
+});
